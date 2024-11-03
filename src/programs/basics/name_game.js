55b2251e5bc90a601fc0ee1aec1cf84bb51070e6
Andrea Grasso <desk@feelfetch.xyz>
2024-11-03 13:55:13 +0100

    .

--- src/programs/basics/name_game.js
+++ src/programs/basics/name_game.js
@@ -8,22 +8,24 @@
 // The default backround color and font attributes can be altered
 // by exporting a ‘settings’ object (see the manual for details).
 export const settings = {
-	backgroundColor : 'black',
-	color : 'white',
+	fps : 60,
+	backgroundColor : 'white',
+	color : 'black',
 	fontWeight : 100, // or ‘lighter’
-	fontSize : '3em'
+	fontSize : '14px',
+	fontFamily : 'IBMPlexMono-Light'
 }
 
-const TAU = Math.PI * 2
+const TAU = Math.PI * 1
 
 export function main(coord, context, cursor, buffer) {
-	const a = context.frame * 0.05
-	const f = Math.floor((1 - Math.cos(a)) * 10) + 1
+	const a = context.frame * 0.08
+	const f = Math.floor((1 - Math.cos(a)) * 10) + 2
 	const g = Math.floor(a / TAU) % 10 + 1
 	const i = coord.index % (coord.y * g + 1) % (f % context.cols)
 	// NOTE: If the function returns ‘undefined’ or ‘null’
 	// a space character will be inserted.
 	// In some cases ‘i’ may be greater than 2:
 	// JavaScript array out of bounds results in ‘undefined’.
-	return 'Ada'[i]
+	return 'WRDLSS'[i]
 }
